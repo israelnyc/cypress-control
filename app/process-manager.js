107a405da7ac6acee83e34c580e6683998e13124
Israel Schulman <israel.schulman@gmail.com>
2021-02-27 16:00:14 -0500

    Kill cypress process whether it's starting or running

--- app/process-manager.js
+++ app/process-manager.js
@@ -1,34 +1,34 @@
-const { getStatusFromServer, resetProcessStatus } = require('./status')
+const { getStatusFromServer, resetProcessStatus } = require('./status');
 
 async function killCypressProcess() {
-    const status = await getStatusFromServer()
-    
-    if(status.isRunning && status.cypressPID) {
-        process.kill(status.cypressPID)
+    const status = await getStatusFromServer();
 
-        resetProcessStatus()
+    if ((status.isRunning || status.isStarting) && status.cypressPID) {
+        process.kill(status.cypressPID);
+
+        resetProcessStatus();
     }
 }
 
 function handleSIGINT() {
-    if(process.platform === "win32") {
-        const rl = require("readline").createInterface({
+    if (process.platform === 'win32') {
+        const rl = require('readline').createInterface({
             input: process.stdin,
-            output: process.stdout
-        })
-    
-        rl.on("SIGINT", function() {
-            process.emit("SIGINT")
-        })
+            output: process.stdout,
+        });
+
+        rl.on('SIGINT', function () {
+            process.emit('SIGINT');
+        });
     }
-    
-    process.on('SIGINT', function() {
-        resetProcessStatus()
-        process.exit()
-    })
+
+    process.on('SIGINT', function () {
+        resetProcessStatus();
+        process.exit();
+    });
 }
 
 module.exports = {
     killCypressProcess,
-    handleSIGINT
-}
+    handleSIGINT,
+};
