36bc238c0666c5d40f123f55261c52a69ccf38c3
Israel Schulman <israel.schulman@gmail.com>
2021-03-15 11:36:09 -0400

    Prettier formatting

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,26 +1,27 @@
-import io from 'socket.io-client'
-import events from './status-events'
+import io from 'socket.io-client';
+import events from './status-events';
 
 export function getSocket() {
-    const socketURL = `http://${window.location.hostname}:8686`
-    
-    window.cypressDashboardSocket = window.cypressDashboardSocket || io(socketURL)
+    const socketURL = `http://${window.location.hostname}:8686`;
 
-    if(process.env.NODE_ENV === 'test') {
-        window.cypressDashboardSocket.disconnect()
+    window.cypressDashboardSocket =
+        window.cypressDashboardSocket || io(socketURL);
+
+    if (process.env.NODE_ENV === 'test') {
+        window.cypressDashboardSocket.disconnect();
     }
 
-    return window.cypressDashboardSocket
+    return window.cypressDashboardSocket;
 }
 
 export function startCypressRunner() {
-    const socket = getSocket()
-    console.log('Starting runner')
-    socket.emit(events.CYPRESS_DASHBOARD_START_RUNNER)
+    const socket = getSocket();
+    console.log('Starting runner');
+    socket.emit(events.CYPRESS_DASHBOARD_START_RUNNER);
 }
 
 export function stopCypressRunner() {
-    const socket = getSocket()
-    console.log('Stopping runner')
-    socket.emit(events.CYPRESS_DASHBOARD_STOP_RUNNER)
+    const socket = getSocket();
+    console.log('Stopping runner');
+    socket.emit(events.CYPRESS_DASHBOARD_STOP_RUNNER);
 }
